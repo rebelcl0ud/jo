918b01fb6ca27a712aca493c2a2812c35ab4a2de
jovanka <jovanka.placeres@gmail.com>
2017-05-29 22:00:35 +0000

    picked up where left off-- functional contact form, edit about and footer, updated model/controller, no need for db, added another vid and adjusted padding on css and adj grid/sections on index

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,16 +1,22 @@
 class ContactMailer < ActionMailer::Base
   # contact email
-  default to: "rebelcl0udmedia@gmail.com"
+  default from: "no-reply@rebelcl0ud.herokuapp.com"
 
 
 # what is being sent from contact form
-  def contact_email(name, email, message)
-    @name = name
-    @email = email
-    @message = message
-    mail(from: email, subject: 'Contact Request')
+  def contact_email(contact)
+   @contact = contact
+   mail(to: ENV['GMAIL_ADDRESS'], 
+        subject: 'Contact Form Inquiry',
+        message: @contact.message)
   end
 
-
-
+# what is received by person using contact form
+  def message_sent(contact)
+    @contact = contact
+    mail(to: @contact.email, 
+         subject: "Contact Form Received",
+         message: @contact.message)
+  end
 end
+
